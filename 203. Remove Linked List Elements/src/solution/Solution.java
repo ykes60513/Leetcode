c9a74590866f95a2ec68d3529ba92966136734c8
ykes60513 <r01921040@gmail.com>
2019-06-03 20:03:14 -0400

    203. Remove Linked List Elements

---
+++ 203. Remove Linked List Elements/src/solution/Solution.java
@@ -0,0 +1,26 @@
+package solution;
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+	}
+}
+class Solution {
+	public ListNode removeElements(ListNode head, int val) {
+
+		ListNode dummy = new ListNode(0);
+		dummy.next = head;
+		ListNode cur = dummy;
+
+		while (cur.next != null) {
+			if (cur.next.val == val) {
+				cur.next = cur.next.next;
+			} else
+				cur = cur.next;
+		}
+		return dummy.next;
+	}
+}
