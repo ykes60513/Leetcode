de0940498a16dab8af66d79094d3569268b55669
ykes60513 <r01921040@gmail.com>
2019-06-04 20:03:36 -0400

    708. Insert into a Cyclic Sorted List

---
+++ 708. Insert into a Cyclic Sorted List/src/solution/Solution.java
@@ -0,0 +1,47 @@
+package solution;
+
+class Node {
+	public int val;
+	public Node next;
+
+	public Node(int _val) {
+		val = _val;
+	}
+
+	public Node(int _val, Node _next) {
+		val = _val;
+		next = _next;
+	}
+}
+
+class Solution {
+	public Node insert(Node start, int x) {
+		if (start == null) {
+			Node cur = new Node(x);
+			cur.next = cur;
+			return cur;
+		}
+		Node cur = start.next;
+		Node prev = start;
+		boolean find = false;
+		while (cur != start) {
+			int next = cur.val;
+			int pre = prev.val;
+			if (x == pre || x <= next && x > pre || next < pre && x > pre || next < pre && x < next) {
+				find = true;
+				Node node = new Node(x);
+				prev.next = node;
+				node.next = cur;
+				break;
+			}
+			prev = cur;
+			cur = cur.next;
+		}
+		if (!find) {
+			Node node = new Node(x);
+			prev.next = node;
+			node.next = cur;
+		}
+		return start;
+	}
+}
