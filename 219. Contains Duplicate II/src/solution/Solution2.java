3aac99ad3eeee73cc04e8458058fe5e227ee8751
ykes60513 <r01921040@gmail.com>
2019-06-06 17:46:12 -0400

    219. Contains Duplicate II

---
+++ 219. Contains Duplicate II/src/solution/Solution2.java
@@ -0,0 +1,23 @@
+package solution;
+
+import java.util.*;
+
+public class Solution2 {
+	public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+		Set<Integer> set = new HashSet<>();
+
+		for (int i = 0; i < nums.length; i++) {
+
+			if (i > k)
+				set.remove(nums[i - k - 1]);
+
+			if (set.contains(nums[i]))
+				return true;
+
+			set.add(nums[i]);
+
+		}
+		return false;
+	}
+}
