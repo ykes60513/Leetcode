a8addf86762871439f8ca25fa1cbc3284ee629b0
ykes60513 <r01921040@gmail.com>
2019-06-17 00:48:24 -0400

    739. Daily Temperatures

--- 739. Daily Temperatures/src/solution/Solution1.java
+++ 739. Daily Temperatures/src/solution/Solution1.java
@@ -4,12 +4,16 @@ import java.util.*;
 
 public class Solution1 {
 	public int[] dailyTemperatures(int[] T) {
+		
 		int[] ans = new int[T.length];
 		Stack<Integer> stack = new Stack<>();
-		for (int i = T.length - 1; i >= 0; --i) {
-			while (!stack.isEmpty() && T[i] >= T[stack.peek()])
-				stack.pop();
-			ans[i] = stack.isEmpty() ? 0 : stack.peek() - i;
+		
+		for (int i = 0; i < T.length; i++) {
+			while (!stack.isEmpty() && T[i] > T[stack.peek()]) {
+				int temp = stack.pop();
+				ans[temp] = i - temp;
+				
+			}
 			stack.push(i);
 		}
 		return ans;
