790bc783877afba9ad579d1a7a81ca4208099b70
ykes60513 <r01921040@gmail.com>
2019-06-17 00:19:29 -0400

    739. Daily Temperatures

---
+++ 739. Daily Temperatures/src/solution/Solution2.java
@@ -0,0 +1,21 @@
+package solution;
+
+public class Solution2 {
+	public int[] dailyTemperatures(int[] T) {
+
+		int[] stack = new int[T.length];
+		int top = -1;
+		int[] result = new int[T.length];
+
+		for (int i = 0; i < T.length; i++) {
+			while (top > -1 && T[i] > T[stack[top]]) {
+				int temp = stack[top--];
+				result[temp] = i - temp;
+			}
+			stack[++top] = i;
+		}
+
+		return result;
+
+	}
+}
