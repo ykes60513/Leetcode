b54f1aac08889104868f163bcc666434d3b53479
ykes60513 <r01921040@gmail.com>
2019-06-18 16:55:16 -0400

    90. Subsets II

---
+++ 90. Subsets II/src/solution/Solution.java
@@ -0,0 +1,30 @@
+package solution;
+
+import java.util.*;
+
+public class Solution {
+	public List<List<Integer>> subsetsWithDup(int[] nums) {
+
+		Arrays.sort(nums);
+		List<List<Integer>> res = new ArrayList<>();
+		helper(res, new ArrayList<Integer>(), nums, 0);
+
+		return res;
+
+	}
+
+	private void helper(List<List<Integer>> res, List<Integer> list, int[] nums, int start) {
+
+		res.add(new ArrayList<>(list));
+		for (int i = start; i < nums.length; i++) {
+
+			if (i > start && nums[i] == nums[i - 1])
+				continue;
+			list.add(nums[i]);
+			helper(res, list, nums, i + 1);
+			list.remove(list.size() - 1);
+
+		}
+
+	}
+}
