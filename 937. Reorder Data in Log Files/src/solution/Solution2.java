f455ac61c31433e71a1ea984dd051caae0d185b7
ykes60513 <r01921040@gmail.com>
2019-12-13 20:28:40 -0500

    937. Reorder Data in Log Files

---
+++ 937. Reorder Data in Log Files/src/solution/Solution2.java
@@ -0,0 +1,26 @@
+package solution;
+
+import java.util.Arrays;
+
+public class Solution2 {
+	public String[] reorderLogFiles(String[] logs) {
+
+		Arrays.sort(logs, (s1, s2) -> {
+
+			String[] split1 = s1.split(" ", 2);
+			String[] split2 = s2.split(" ", 2);
+			boolean isDigit1 = Character.isDigit(split1[1].charAt(0));
+			boolean isDigit2 = Character.isDigit(split2[1].charAt(0));
+			if (!isDigit1 && !isDigit2) {
+				int comp = split1[1].compareTo(split2[1]);
+				if (comp != 0)
+					return comp;
+				return split1[0].compareTo(split2[0]);
+			}
+			return isDigit1 ? (isDigit2 ? 0 : 1) : -1;
+		});
+
+		return logs;
+	}
+
+}
