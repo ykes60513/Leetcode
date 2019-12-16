6b00ca7fd45a3f290fe1ca38b4576fa02f834706
ykes60513 <r01921040@gmail.com>
2019-12-16 02:07:35 -0500

    250. Count Univalue Subtrees

---
+++ 250. Count Univalue Subtrees/src/solution/Solution1.java
@@ -0,0 +1,26 @@
+package solution;
+
+import main.TreeNode;
+
+public class Solution1 {
+	int count;
+
+	public int countUnivalSubtrees(TreeNode root) {
+		count = 0;
+		helper(root, Integer.MAX_VALUE);
+		return count;
+	}
+
+	private boolean helper(TreeNode n, int v) {
+		if (n == null) {
+			return true;
+		}
+		boolean left = helper(n.left, n.val);
+		boolean right = helper(n.right, n.val);
+		if (left && right) {
+			count++;
+			return n.val == v;
+		}
+		return false;
+	}
+}
---
+++ 250. Count Univalue Subtrees/src/solution/Solutoin2.java
@@ -0,0 +1,45 @@
+package solution;
+
+import main.TreeNode;
+
+public class Solutoin2 {
+	int count = 0;
+
+	public int countUnivalSubtrees(TreeNode root) {
+		if (root == null)
+			return 0;
+		check(root);
+		return count;
+	}
+
+	private boolean check(TreeNode subtree) {
+		if (subtree.left == null && subtree.right == null) {
+			count++;
+			return true;
+		}
+		boolean flag = true;
+		if (subtree.left != null) {
+			if (check(subtree.left)) {
+				if (subtree.val != subtree.left.val) {
+					flag = false;
+				}
+			} else {
+				flag = false;
+			}
+		}
+		if (subtree.right != null) {
+			if (check(subtree.right)) {
+				if (subtree.val != subtree.right.val) {
+					flag = false;
+				}
+			} else {
+				flag = false;
+			}
+		}
+		if (flag == false) {
+			return false;
+		}
+		count++;
+		return true;
+	}
+}
