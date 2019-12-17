3a2b940466fdb5306de532629ee7c4007c66ca38
ykes60513 <r01921040@gmail.com>
2019-12-16 23:54:57 -0500

    344. Reverse String

---
+++ 344. Reverse String/src/solution/Solution.java
@@ -0,0 +1,21 @@
+package solution;
+
+public class Solution {
+	public void reverseString(char[] s) {
+
+		if (s == null || s.length <= 0)
+			return;
+
+		int i = 0;
+		int j = s.length - 1;
+		while (i < j) {
+
+			char temp = s[i];
+			s[i] = s[j];
+			s[j] = temp;
+			i++;
+			j--;
+		}
+
+	}
+}
