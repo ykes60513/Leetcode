00525e508539e73bfb597b36258d7747e4ecff15
ykes60513 <r01921040@gmail.com>
2019-12-16 19:16:15 -0500

    283. Move Zeroes

---
+++ 283. Move Zeroes/src/solution/Solution.java
@@ -0,0 +1,24 @@
+package solution;
+
+public class Solution {
+	public void moveZeroes(int[] nums) {
+
+		if (nums == null || nums.length <= 0)
+			return;
+
+		int i = 0;
+		int j = 0;
+		while (i < nums.length) {
+
+			if (nums[i] != 0) {
+				nums[j++] = nums[i];
+			}
+			i++;
+
+		}
+		while (j < nums.length) {
+			nums[j++] = 0;
+		}
+
+	}
+}
