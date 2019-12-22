57edc467340cb5f0f5a3359a48cd26d9cc085e8e
ykes60513 <r01921040@gmail.com>
2019-12-21 19:04:58 -0500

    290. Word Pattern

---
+++ 290. Word Pattern/src/solution/Solution.java
@@ -0,0 +1,34 @@
+package solution;
+
+import java.util.*;
+
+public class Solution {
+	public boolean wordPattern(String pattern, String str) {
+
+		Map<Character, String> map = new HashMap<>();
+		int clen = pattern.length();
+		String[] s = str.split(" ");
+		if (clen != s.length)
+			return false;
+
+		for (int i = 0; i < clen; i++) {
+
+			char c = pattern.charAt(i);
+			if (!map.containsKey(c)) {
+				if (!map.containsValue(s[i]))
+					map.put(c, s[i]);
+
+				else
+					return false;
+			} else {
+
+				if (!map.get(c).equals(s[i]))
+					return false;
+			}
+
+		}
+
+		return true;
+
+	}
+}
