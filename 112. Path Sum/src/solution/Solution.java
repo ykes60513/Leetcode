85a16e775238567ed4b3da4360526deff852eab2
ykes60513 <r01921040@gmail.com>
2019-12-22 01:14:11 -0500

    112. Path Sum

---
+++ 112. Path Sum/src/solution/Solution.java
@@ -0,0 +1,21 @@
+package solution;
+
+import main.TreeNode;
+
+public class Solution {
+	public boolean hasPathSum(TreeNode root, int sum) {
+
+		if (root == null)
+			return false;
+
+		if (root.left == null && root.right == null) {
+
+			return root.val == sum;
+		} else {
+
+			return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+		}
+
+	}
+
+}
