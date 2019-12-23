6b2410ec06b93aa9e4056a1bae8272d67ff039cb
ykes60513 <r01921040@gmail.com>
2019-12-23 11:51:48 -0500

    167. Two Sum II - Input array is sorted

---
+++ 167. Two Sum II - Input array is sorted/src/solution/Solution.java
@@ -0,0 +1,17 @@
+package solution;
+
+public class Solution {
+	public int[] twoSum(int[] numbers, int target) {
+
+		int first = 0, last = numbers.length - 1;
+
+		while (numbers[first] + numbers[last] != target) {
+			if (numbers[first] + numbers[last] > target)
+				last--;
+			else
+				first++;
+		}
+		return new int[] { first + 1, last + 1 };
+
+	}
+}
