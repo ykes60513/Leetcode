4fbf7eeccc0f601682d1a4aad3f90714cfe1f147
ykes60513 <r01921040@gmail.com>
2019-12-28 01:58:16 -0500

    443. String Compression

---
+++ Testing/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println(1^3);
+		System.out.println(5|6);
+		System.out.println(true^false);
+		System.out.println(true|false);
+	}
+
+}
