7f7e9cf76840bb29a7676f9b6082d2dec2242d9f
ykes60513 <r01921040@gmail.com>
2019-12-28 05:58:42 -0500

    266. Palindrome Permutation

---
+++ 266. Palindrome Permutation/src/solution/Solution.java
@@ -0,0 +1,23 @@
+package solution;
+
+public class Solution {
+	public boolean canPermutePalindrome(String s) {
+
+		int[] alpha = new int[128];
+		char[] cArr = s.toCharArray();
+
+		for (char c : cArr) {
+
+			if (alpha[c] > 0)
+				alpha[c]--;
+			else
+				alpha[c]++;
+		}
+		int num = 0;
+		for (int i : alpha) {
+			num += i;
+		}
+
+		return (num == 0 || num == 1);
+	}
+}
--- 3. Longest Substring Without Repeating Characters/src/solution/Solution1.java
+++ 3. Longest Substring Without Repeating Characters/src/solution/Solution1.java
@@ -4,12 +4,13 @@ public class Solution1 {
 	
 	public int lengthOfLongestSubstring(String s) {
 		int n = s.length(), ans = 0;
-		int[] index = new int[128]; // current index of character
+		int[] index = new int[128]; 
+		// current index of character
 		// try to extend the range [i, j]
-		for (int j = 0, i = 0; j < n; j++) {
-			i = Math.max(index[s.charAt(j)], i);
-			ans = Math.max(ans, j - i + 1);
-			index[s.charAt(j)] = j + 1;
+		for (int hi = 0, lo = 0; hi < n; hi++) {
+			lo = Math.max(index[s.charAt(hi)], lo);
+			ans = Math.max(ans, hi - lo + 1);
+			index[s.charAt(hi)] = hi + 1;
 		}
 		return ans;
 	}
