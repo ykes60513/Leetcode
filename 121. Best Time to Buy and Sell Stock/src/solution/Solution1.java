8dce087b3966166f800dc9aeef69dc1bc8225207
ykes60513 <r01921040@gmail.com>
2020-09-21 02:59:40 -0400

    121. Best Time to Buy and Sell Stock

--- 121. Best Time to Buy and Sell Stock/src/solution/Solution1.java
+++ 121. Best Time to Buy and Sell Stock/src/solution/Solution1.java
@@ -2,17 +2,20 @@ package solution;
 
 public class Solution1 {
 
-	 public int maxProfit(int[] prices) {
-	        
-	        int minprice = Integer.MAX_VALUE;
-	        int maxprofit = 0;
-	        for (int i = 0; i < prices.length; i++) {
-	            if (prices[i] < minprice)
-	                minprice = prices[i];
-	            else if (prices[i] - minprice > maxprofit)
-	                maxprofit = prices[i] - minprice;
-	        }
-	        return maxprofit;
-	    }
-	
+	public int maxProfit(int[] prices) {
+
+		int min = Integer.MAX_VALUE;
+		int profit = 0;
+
+		for (int i = 0; i < prices.length; i++) {
+
+			if (prices[i] < min)
+				min = prices[i];
+
+			profit = Math.max(profit, prices[i] - min);
+		}
+
+		return profit;
+	}
+
 }
