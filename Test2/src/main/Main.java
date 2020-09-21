4516f1dcdde5bc77a3224b786cdb40e0dfe765ab
ykes60513 <r01921040@gmail.com>
2020-09-21 01:49:00 -0400

    55. Jump Game

--- Test2/src/main/Main.java
+++ Test2/src/main/Main.java
@@ -2,6 +2,40 @@ package main;
 
 import java.io.*;
 
+public class Main {
+
+	public static int[] Convert(String s) {
+
+		s = s.trim().substring(1, s.length() - 1);
+		if (s.length() == 0) {
+			return new int[0];
+		}
+
+		String[] number = s.split(",");
+		int[] output = new int[number.length];
+
+		int i = 0;
+		while (i < number.length) {
+			output[i] = Integer.parseInt(number[i].trim());
+			i++;
+		}
+		return output;
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		String line;
+		while ((line = in.readLine()) != null) {
+			int[] numbers = Convert(line);
+
+			int result = new Solution().area(numbers);
+			System.out.println(result);
+		}
+	}
+}
+
 class Solution {
 	public int area(int[] input) {
 
@@ -20,40 +54,5 @@ class Solution {
 		}
 
 		return result;
-	}
-}
-
-public class Main {
-
-	public static int[] Convert(String s) {
-
-		s = s.trim().substring(1, s.length() - 1);
-		if (s.length() == 0) {
-			return new int[0];
-		}
-
-		String[] number = s.split(",");
-		int[] output = new int[number.length];
-		
-
-		int i = 0;
-		while (i < number.length) {
-			output[i] = Integer.parseInt(number[i].trim());
-			i++;
-		}
-		return output;
-	}
-
-	public static void main(String[] args) throws IOException {
-
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
-		String line;
-		while ((line = in.readLine()) != null) {
-			int[] height = Convert(line);
-
-			int result = new Solution().area(height);
-			System.out.println(result);
-		}
 	}
 }
