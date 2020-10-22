f6eec645926b34ccf6041045df919dcc80221671
ykes60513 <r01921040@gmail.com>
2020-10-22 13:47:06 -0400

    268. Missing Number

---
+++ 268. Missing Number/src/solution/Solution.java
@@ -0,0 +1,19 @@
+package solution;
+
+public class Solution {
+
+	public int missingNumber(int[] nums) {
+
+		int res = 0;
+
+		for (int i = 1; i <= nums.length; i++) {
+
+			res ^= i;
+		}
+		for (int i = 0; i < nums.length; i++) {
+			
+			res ^= nums[i];
+		}
+		return res;
+	}
+}
