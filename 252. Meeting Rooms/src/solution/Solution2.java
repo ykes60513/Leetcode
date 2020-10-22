3a98a071e00e7ce67a5e8b56208df34f47def0af
ykes60513 <r01921040@gmail.com>
2020-10-22 03:57:04 -0400

    253. Meeting Rooms II

---
+++ 252. Meeting Rooms/src/solution/Solution2.java
@@ -0,0 +1,37 @@
+/*******************************
+ * Chronological Ordering
+ */
+
+package solution;
+
+import java.util.*;
+
+public class Solution2 {
+	public int minMeetingRooms(int[][] intervals) {
+
+		if (intervals.length == 0) {
+			return 0;
+		}
+
+		int[] starts = new int[intervals.length];
+		int[] ends = new int[intervals.length];
+		
+		for (int i = 0; i < intervals.length; i++) {
+			starts[i] = intervals[i][0];
+			ends[i] = intervals[i][1];
+		}
+		
+		Arrays.sort(starts);
+		Arrays.sort(ends);
+		int rooms = 0, endsItr = 0;
+		for (int i = 0; i < starts.length; i++) {
+			if (starts[i] < ends[endsItr]) {
+				rooms++;
+			} else {
+				endsItr++;
+			}
+		}
+		return rooms;
+
+	}
+}
---
+++ 253. Meeting Rooms II/src/solution/Solution.java
@@ -0,0 +1,48 @@
+/****************************************
+ * PrioritiQueue
+ */
+
+package solution;
+
+import java.util.*;
+
+public class Solution {
+
+	public int minMeetingRooms(int[][] intervals) {
+
+		if (intervals.length == 0) {
+			return 0;
+		}
+
+		// Sort intervals by start time
+		Arrays.sort(intervals, new Comparator<int[]>() {
+
+			@Override
+			public int compare(int[] a, int[] b) {
+				return a[0] - b[0];
+			}
+		});
+
+		PriorityQueue<Integer> queue = new PriorityQueue<>(intervals.length, new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer a, Integer b) {
+
+				return a - b;
+			}
+		});
+
+		queue.add(intervals[0][1]);
+
+		for (int i = 1; i < intervals.length; i++) {
+
+			if (intervals[i][0] >= queue.peek()) {
+				queue.poll();
+			}
+
+			queue.add(intervals[i][1]);
+		}
+
+		return queue.size();
+	}
+}
