ef832565f2a609b9c3f31ef1bd937a4c3be7ff1b
ykes60513 <r01921040@gmail.com>
2021-09-21 15:02:24 +0800

    96. Unique Binary Search Trees

--- 96. Unique Binary Search Trees/src/solution/Solution1.java
+++ 96. Unique Binary Search Trees/src/solution/Solution1.java
@@ -1,5 +1,10 @@
 /****************************
+ * Given an integer n, return the number of structurally unique BST's (binary search trees) 
+ * which has exactly n nodes of unique values from 1 to n.
+ * https://leetcode.com/problems/unique-binary-search-trees/
+ * 
  * Dynamic Programming
+ * G(n) = Sigma G(i-1)*G(n-i)
  */
 package solution;
 
@@ -8,8 +13,8 @@ public class Solution1 {
 		int[] G = new int[n + 1];
 		G[0] = G[1] = 1;
 
-		for (int i = 2; i <= n; ++i) {
-			for (int j = 1; j <= i; ++j) {
+		for (int i = 2; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
 				G[i] += G[j - 1] * G[i - j];
 			}
 		}
